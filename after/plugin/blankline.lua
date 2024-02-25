f681a54631fcdc986e6697ee2a57eb4f8d562e4f
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2024-02-25 00:46:04 -0300

    Update blankline highlights

--- after/plugin/blankline.lua
+++ after/plugin/blankline.lua
@@ -1,10 +1,45 @@
 vim.opt.termguicolors = true
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#524f67 gui=nocombine]]
+--
 
-require("indent_blankline").setup {
-  show_end_of_line = true,
-  show_current_context = true,
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-  },
+-- require("ibl").setup {
+  -- show_end_of_line = true,
+  -- show_current_context = true,
+  -- char_highlight_list = {
+  --   "IndentBlanklineIndent1",
+  -- },
+-- }
+local highlight = {
+    "RainbowRed",
 }
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+
+require("ibl").setup {
+    indent = {
+        -- tab_char = { "a", "b", "c" },
+        -- highlight = { "Function", "Label" },
+        smart_indent_cap = true,
+        priority = 2,
+    },
+    whitespace = {
+        -- highlight = { "Whitespace", "NonText" }, 
+        remove_blankline_trail = false
+    },
+    scope = {
+        show_start = false,
+        show_end = false,
+        highlight = highlight,
+        node_type = { lua = { "block", "chunk" } },
+        priority = 500
+    }
+}
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
